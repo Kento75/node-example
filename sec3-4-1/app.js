51fbe8a8c7df1c8ba00a90b94a2f1f97cf3ed096
root <root@DESKTOP-EJAE17D.localdomain>
2018-06-09 17:33:38 +0900

    ページ追加

--- sec3-4-1/app.js
+++ sec3-4-1/app.js
@@ -62,37 +62,22 @@ function response_index(request, response) {
   response.end()
 }
 
+var data2 = {
+  'Taro': ['taro@yamada', '09-999-999', 'Tokyo'],
+  'Hanako': ['hanako@flower', '080-888-888', 'Yokohama'],
+  'Sachiko': ['sachiko@happy', '070-777-777', 'Nagoya'],
+  'Ichiro': ['ichi@baseball', '060-666-666', 'USA'],
+}
+
 function response_other(request, response) {
   var msg = 'これはotherぺージ'
-  // POSTアクセス時の処理
-  if (request.method == 'POST') {
-    var body = ''
-
-    // データ受信のイベント処理
-    request.on('data', (data) => {
-      body += data
-    })
-  // データ受信終了後のイベント処理
-    request.on('end', () => {
-      var post_data = qs.parse(body)
-      msg += 'あなたは「' + post_data.msg + '」と書きました'
-      var content = ejs.render(other_page, {
-        title: 'Ohter',
-        content: msg,
-      })
-      response.writeHead(200, {'Content-Type': 'text/html'})
-      response.write(content)
-      response.end()
-    })
-  // GETアクセス時の処理
-  } else {
-    var msg = 'ページがありません'
-    var content = ejs.render(other_page, {
-      title: 'Other',
-      content: msg,
-    })
-    response.writeHead(200, {'Content-Type': 'text/html'})
-    response.write(content)
-    response.end()
-  }
+  var content = ejs.render(other_page, {
+    title: 'Other',
+    content: msg,
+    data: data2,
+    filename: 'data_item'
+  })
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.write(content)
+  response.end()
 }
